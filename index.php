180aa74b0b05945093f082de02ce5102f240b89d
Noah McGraw <McMoldy96@gmail.com>
2018-06-23 13:42:03 -0500

    Established general format for html template. Ctrl logic is set for first question; ctrl logic will be applied to following questions in next commit.

--- index.php
+++ index.php
@@ -24,53 +24,119 @@
   </head>
   <body ng-app="app" ng-cloak>
     <div class="mainContainer" ng-controller='mainCtrl'>
-        <header layout="row"
-                layout-align="space-around center">
+        <header class="md-horizontal-padding"
+                layout="row"
+                layout-align="space-between center">
             <img src="\app\assets\images\unisynLogoSmall.png" class="md-padding" height="120px" width="120px">
-            <label class="md-display-3">Private Policy Generator</label>
+            <md-button class="md-primary">More Tools</md-button>
         </header>
         <md-divider></md-divider>
-        <div class="initialInfoBG"layout="row" layout-align="center center">
-            <md-content class="md-block md-padding initialInfoBG" style="width: 600px; max-width: 100%;">
-                <label class="md-headline">Privacy Policy Made Simple.</label>
-                <p>Let us do the hard work for you! With our free <strong>Private Policy Generator</strong>,
-                <br>
-                 you can have a custom policy in your inbox in as little as <strong>Five Minutes</strong>!
-                </p>
+        <div class="initialInfoBG" layout="column" layout-gt-sm="row" layout-align="space-around center">
+            <md-content class="md-block md-padding md-margin initialInfoBG">
+                <label class="md-display-3">Privacy Policy Generator</label>
+                <div ng-hide="newPolicy.questionaire">
+                    <p>Let us do the hard work for you! With our free <strong>Privacy Policy Generator</strong>,
+                    <br>
+                    you can have a custom policy on your desk in as little as <strong>Five Minutes</strong>!
+                    </p>
+                </div>
+                <md-button class="md-primary md-raised" ng-click="startNewPolicy()" ng-hide="newPolicy.addingInfo || newPolicy.questionaire">Let's Get Started</md-button>
             </md-content>
-            <md-content class="md-block md-padding" style="width: 600px; max-width: 100%;">
-                <form name="initialInfo" ng-submit="initialInfoFormSubmit()">
+        </div>
+        <div layout="column" layout-align="center center">
+            <md-content class="md-block md-padding md-vertical-margin privacyPolicyGeneratorContainer"
+                        ng-show="newPolicy.addingInfo">
+                <form name="initialInfoForm" ng-submit="showQuestionnaire()">
                     <md-toolbar class="md-primary">
                         <div class="md-toolbar-tools" style="text-align: center;">
-                            <label class="md-headline" flex md-truncate>Just Some Basic Info...</label>
+                            <label class="md-headline" flex>Just Some Basic Info...</label>
                         </div>
                     </md-toolbar>
-                    <md-input-container style="width: 300px;">
-                        <label>Company Name</label>
-                        <input ng-model="newPolicy.companyName" name="companyName"  ng-required="true">
-                    </md-input-container>
-                    <md-input-container style="width: 200px;">
-                        <label>Email Address</label>
-                        <input ng-model="newPolicy.email" name="email"  ng-required="true">
-                    </md-input-container>
+                    <div layout="column" layout-gt-sm="row">
+                        <md-input-container flex="50">
+                            <label>Company Name</label>
+                            <input ng-model="newPolicy.companyName" name="companyName"  ng-required="true">
+                        </md-input-container>
+                        <md-input-container flex="50">
+                            <label>Email Address</label>
+                            <input ng-model="newPolicy.email" name="email"  ng-required="true">
+                        </md-input-container>
+                    </div>
                     <md-input-container class="md-block">
                         <label>Address</label>
                         <input ng-model="newPolicy.companyAddress.street" name="companyAddressStreet"  ng-required="true">
                     </md-input-container>
-                    <md-input-container>
-                        <label>City</label>
-                        <input ng-model="newPolicy.companyAddress.city" name="companyAddressCity"  ng-required="true">
+                    <div layout="column" layout-gt-sm="row">
+                        <md-input-container flex="30">
+                            <label>City</label>
+                            <input ng-model="newPolicy.companyAddress.city" name="companyAddressCity"  ng-required="true">
+                        </md-input-container>
+                        <md-input-container flex="20">
+                            <label>State/Province</label>
+                            <input ng-model="newPolicy.companyAddress.state" name="companyAddressState"  ng-required="true">
+                        </md-input-container>
+                    </div>
+                    <div layout="column" layout-gt-sm="row">
+                            <md-input-container flex="50">
+                            <label>Zip Code</label>
+                            <input ng-model="newPolicy.companyAddress.zip" name="companyAddressZip"  ng-required="true">
+                        </md-input-container>
+                        <md-input-container flex="50">
+                            <label>Country</label>
+                            <input ng-model="newPolicy.companyAddress.country" name="companyAddressCountry"  ng-required="true">
+                        </md-input-container>
+                    </div>
+                    <md-input-container class="md-block">
+                        <label>Website Url</label>
+                        <input ng-model="newPolicy.companyWebsite" name="companyWebsite"  ng-required="true">
                     </md-input-container>
-                    <md-input-container>
-                        <label>State</label>
-                        <input ng-model="newPolicy.companyAddress.state" name="companyAddressState"  ng-required="true">
-                    </md-input-container>
-                    <md-input-container>
-                        <label>Zip Code</label>
-                        <input ng-model="newPolicy.companyAddress.zip" name="companyAddressZip"  ng-required="true">
-                    </md-input-container>
-                    <md-button class="md-primary md-raised">Begin</md-button>
+                    <md-button type="submit" class="md-primary md-raised">Continue</md-button>
                 </form>
+            </md-content>
+            <md-content class="md-block md-padding md-vertical-margin privacyPolicyGeneratorContainer"
+                        ng-show="newPolicy.questionaire">
+                <md-toolbar>
+                    <div class="md-toolbar-tools" style="text-align: center;">
+                        <label class="md-headline" flex>{{ questionnaireContents[ currentQuestion ].header }}</label>
+                        {{ currentQuestion + 1 }}/{{ totalQuestions }}
+                    </div>
+                </md-toolbar>
+                <br>
+                <div class="md-title">{{ questionnaireContents[ currentQuestion ].subheader }}</div>
+                <br>
+                <div ng-repeat="content in questionnaireContents[ currentQuestion ].content">
+                    <strong><em>"{{ content.title }}"</em></strong>
+                    <br>
+                    <div style="text-indent: 2em;">
+                        {{ content.description() }}
+                    </div>
+                    <br>
+                </div>
+                <div class="md-title">{{ questionnaireContents[ currentQuestion ].question }}</div>
+                <md-radio-group ng-model="questionnaireContents[ currentQuestion ].answer"
+                                class="md-vertical-padding"
+                                layout="row"
+                                layout-align="space-around center">
+                    <md-radio-button value="true">Yes</md-radio-button>
+                    <md-radio-button value="false">No</md-radio-button>
+                </md-radio-group>
+                <div ng-show="questionnaireContents[ currentQuestion ].answer == 'true'">
+                    <div class="md-title">Please provide a brief description</div>
+                    <md-input-container class="md-block" flex>
+                        <label>Description</label>
+                        <textarea ng-model="questionnaireContents[ currentQuestion ].answerDescription"></textarea>
+                    </md-input-container>
+                </div>
+                <md-button class="md-primary md-raised"
+                           ng-disabled="questionnaireContents[ currentQuestion ].answer == 'true' &&
+                                        questionnaireContents[ currentQuestion ].answerDescription == ''"
+                           ng-click=goToNextQuestion()>
+                    Next
+                </md-button>
+                <md-button class="md-secondary md-raised"
+                           ng-click=goToLastQuestion()>
+                    Back
+                </md-button>
             </md-content>
         </div>
     </div>
